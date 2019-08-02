25ae49580d7fda06b5eb385c0bbd9ed006ac599d
Yuriy Tsemashko <y.tsemashko@gmail.com>
2019-08-02 11:12:08 +0300

    Refactorm piqad command to have less os.Exits

--- cmd/piqad/main.go
+++ cmd/piqad/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -10,34 +11,68 @@ import (
 )
 
 type args struct {
-	source string
-	input  string
+	from  string
+	input string
 }
 
 func main() {
-	args := parseArgs()
-	var piqadString string
-
-	switch args.source {
-	case "english":
-		var err error
-		piqadString, err = piqad.Transliterate(args.input)
-
-		if err != nil {
-			die(err.Error())
-		}
-	case "piqad":
-		piqadString = args.input
-	default:
-		die(fmt.Sprintf("Invalid -from value. See '%s -h' for details", os.Args[0]))
+	args, err := parseArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
 	}
 
-	codes := toUtf8Codes(piqadString)
+	codes, err := inputToUTF8Codes(args)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
 	fmt.Println(strings.Join(codes, " "))
 }
 
-func parseArgs() args {
-	source := flag.String(
+func normalizeInput(args args) (piqadString string, err error) {
+	switch args.from {
+	case "english":
+		piqadString, err = piqad.Transliterate(args.input)
+		return
+	case "piqad":
+		piqadString = args.input
+		return
+	default:
+		return piqadString, fmt.Errorf("Invalid -from value. See '%s -h' for details", os.Args[0])
+	}
+}
+
+func inputToUTF8Codes(args args) (codes []string, err error) {
+	str, err := normalizeInput(args)
+	if err != nil {
+		return
+	}
+
+	tokenizer := piqad.NewTokenizer(str)
+
+	for tokenizer.Scan() {
+		pchar := tokenizer.Text()
+		code, err := piqad.ToUTF8(pchar)
+
+		if err != nil {
+			return codes, err
+		}
+
+		codes = append(codes, fmt.Sprintf("0x%X", code))
+	}
+
+	if err = tokenizer.Err(); err != nil {
+		return
+	}
+
+	return codes, nil
+}
+
+func parseArgs() (args, error) {
+	from := flag.String(
 		"from",
 		"english",
 		"Sets a source what should be converted to UTF-8 symbols: from \"english\" or from \"piqad\"",
@@ -46,33 +81,7 @@ func parseArgs() args {
 	input := strings.Join(flag.Args(), " ")
 
 	if input == "" {
-		die("You should provide an input")
+		return args{}, errors.New("You should provide an input")
 	}
-	return args{source: *source, input: input}
-}
-
-func die(err string) {
-	fmt.Fprintln(os.Stderr, err)
-	os.Exit(2)
-}
-
-func toUtf8Codes(piqadString string) []string {
-	codes := make([]string, 0)
-	tokenizer := piqad.NewTokenizer(piqadString)
-
-	for tokenizer.Scan() {
-		pchar := tokenizer.Text()
-
-		if code, err := piqad.ToUTF8(pchar); err != nil {
-			die(err.Error())
-		} else {
-			codes = append(codes, fmt.Sprintf("0x%X", code))
-		}
-	}
-
-	if err := tokenizer.Err(); err != nil {
-		die(err.Error())
-	}
-
-	return codes
+	return args{from: *from, input: input}, nil
 }
